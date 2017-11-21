4ae15e4e5cb644188bc20e87d8503bc0191135df
IcusFMagni <danika.ragnhild@gmail.com>
2017-11-21 10:17:29 -0600

    add basic server structure

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE shoes (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR (80),
+	cost INTEGER
+	);
+	
+INSERT INTO shoes (name, cost)
+VALUES ('Puma Soliel V2', 40),
+('Target Garbage Cheepos', 14),
+('Red Wing', 250),
+('Space Boots', 10);
