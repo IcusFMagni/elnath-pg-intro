3b89b425cf698c9ba5e83356e055e25e3ae6db6e
IcusFMagni <danika.ragnhild@gmail.com>
2017-11-21 13:04:42 -0600

    full stack get request

--- server/server.js
+++ server/server.js
@@ -4,10 +4,42 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = 5000;
 
+
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+var config = {
+    database: 'shoestore', //name of database
+    host: 'localhost',//where is your database
+    port: 5432,//port number for database
+    max: 10, //how many connections at one time
+    idleTimeoutMillies: 30000// 30 sec to try and connect
+};
+
+var pool = new pg.Pool(config);
+
+
+
+app.get('/shoes', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase) {
+            console.log('error connecting to database', errorConnectingToDatabase)
+            res.sendStatus(500)
+        } else {
+            client.query('SELECT * FROM shoes', function (errorMakingQuery, result){
+                done();
+                if(errorMakingQuery) {
+                    console.log('query failed', errorMakingQuery)
+                    res.sendStatus(500)
+                } else {
+                    res.send(result.rows);
+                }
+            })
+        }
+
+    })
+})
 
 app.listen(port, function(){
     console.log('listening on port:', port)
 })
-
